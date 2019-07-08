d675be6860c99066099681154a5b3e202f04a3ce
mrholek <lukasz@holeczek.pl>
2019-07-08 18:12:14 +0200

    docs: update vue 3 documentation

---
+++ vue/docs/3.0/assets/js/49.5eab418d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[49],{439:function(t,e,n){"use strict";n.r(e);var i=n(0),r=Object(i.a)({},function(){var t=this.$createElement;return(this._self._c||t)("Api",{attrs:{props:[["tag","'div'","String","Defines the tag of dropdown divider"]]}})},[],!1,null,null,null);e.default=r.exports}}]);
