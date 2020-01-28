929073297dab9e019a6ac0478f50edb43bce7921
woothu <32914662+woothu@users.noreply.github.com>
2020-01-28 16:07:57 +0100

    docs: update vue docs to 3.0.0-beta.10

--- vue/docs/assets/js/70.98f2cb06.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[70],{593:function(t,e,n){"use strict";n.r(e);var s=n(0),a=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("Api",{attrs:{slots:[["default","undefined","Content of the sidebar nav."]]}})}),[],!1,null,null,null);e.default=a.exports}}]);
