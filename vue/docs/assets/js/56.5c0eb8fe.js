05b4a481710cd5465db5b4f49d547fe978e01bc1
woothu <32914662+woothu@users.noreply.github.com>
2020-02-03 17:52:44 +0100

    docs: vue docs fixes version 3.0.0-beta.10

---
+++ vue/docs/assets/js/56.5c0eb8fe.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[56],{622:function(t,e,n){"use strict";n.r(e);var s=n(0),l=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("Api",{attrs:{slots:[["default","undefined","Content of the nav item."]]}})}),[],!1,null,null,null);e.default=l.exports}}]);
