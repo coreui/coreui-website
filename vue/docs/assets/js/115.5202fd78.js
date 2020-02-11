4c95bdb179a8c7c894c76f613d4b9701009150b8
woothu <32914662+woothu@users.noreply.github.com>
2020-02-11 19:18:16 +0100

    docs: vue/docs tabs page fix

---
+++ vue/docs/assets/js/115.5202fd78.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[115],{2214:function(n,e,o){"use strict";o.r(e);o(81);var t={beforeCreate:function(){this.$router.replace("../components/alert",(function(){}),(function(){}))},render:function(n){return n(!1)}},r=o(0),u=Object(r.a)(t,void 0,void 0,!1,null,null,null);e.default=u.exports}}]);
--- vue/docs/assets/js/59.9c5cf772.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[59],{626:function(t,e,n){"use strict";n.r(e);var l=n(0),s=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("Api",{attrs:{props:[["tag","'div'","String","HTML tag of element."]],slots:[["default","undefined","Content of the brand element."]]}})}),[],!1,null,null,null);e.default=s.exports}}]);
