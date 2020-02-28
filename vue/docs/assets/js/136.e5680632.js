8e826cf44bfe238f8f31f91b3e79f233b69499e7
woothu <32914662+woothu@users.noreply.github.com>
2020-02-28 20:12:16 +0100

    docs: vue/docs 3.0.1 update

--- vue/docs/assets/js/136.e5680632.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[136],{473:function(t,s,e){"use strict";e.r(s);var n=e(0),c=Object(n.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("CImg",{staticClass:"mb-2",attrs:{src:"https://picsum.photos/1024/480/?image=54",block:""}})}),[],!1,null,null,null);s.default=c.exports}}]);
