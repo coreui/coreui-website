1fa77b37ab3b49b08a4102918eb246529b67d8ff
woothu <32914662+woothu@users.noreply.github.com>
2020-03-18 17:15:46 +0100

    docs: vue/docs restore previous dropdown page

---
+++ vue/docs/assets/js/117.4f12f69d.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[117],{2221:function(e,n,t){"use strict";t.r(n);t(81);var o={beforeCreate:function(){this.$router.replace("../directives/emit-root-event",(function(){}),(function(){}))},render:function(e){return e(!1)}},r=t(0),i=Object(r.a)(o,void 0,void 0,!1,null,null,null);n.default=i.exports}}]);
--- vue/docs/assets/js/60.b9b7065f.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[60],{559:function(t,n,e){"use strict";e.r(n);var a=e(0),s=Object(a.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("Api",{attrs:{props:[["tag","'ul'","String","HTML tag of the nav."]],slots:[["default","undefined","Content of the nav."]]}})}),[],!1,null,null,null);n.default=s.exports}}]);
