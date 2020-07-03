b4cab54be53ccde4a91073f0e5be95e17ebb475d
woothu <32914662+woothu@users.noreply.github.com>
2020-07-03 16:50:39 +0200

    docs: vue/docs update

---
+++ vue/docs/3.0/assets/js/161.332e3dd6.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[161],{2096:function(e,t,n){"use strict";n.r(t);var s=n(0),r=Object(s.a)({},(function(){var e=this.$createElement;return(this._self._c||e)("CLink",{attrs:{href:"https://coreui.io/vue/demo/#/base/switches",target:"_blank"}},[this._v("\n  Check out examples here!\n")])}),[],!1,null,null,null);t.default=r.exports}}]);
---
+++ vue/docs/3.0/assets/js/61.f6b9a1c1.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[61],{2259:function(t,n,e){"use strict";e.r(n);var a=e(0),s=Object(a.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("Api",{attrs:{props:[["tag","'ul'","String","HTML tag of the nav."]],slots:[["default","undefined","Content of the nav."]]}})}),[],!1,null,null,null);n.default=s.exports}}]);
