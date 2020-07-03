6fe2957847f34b080f29b7abc384c2dc4fb61ac9
woothu <32914662+woothu@users.noreply.github.com>
2020-07-03 21:42:26 +0200

    docs: vue docs update (chose version)

---
+++ vue/docs/assets/js/20.f1ffa2f0.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[20],{2249:function(t,e,n){"use strict";n.r(e);var s=n(0),o=Object(s.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("Api",{attrs:{props:[["tag","'p'","String","Defines the tag."]],slots:[["default","undefined","Set content of the component."]]}})}),[],!1,null,null,null);e.default=o.exports}}]);
