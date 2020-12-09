47ed9fc1802964e22ccb3acd00fb6da6c368f109
mrholek <lukasz@holeczek.pl>
2020-12-09 12:41:32 +0100

    update website

--- demo/3.2.0/rtl/js/datatables.js
+++
@@ -1,9 +0,0 @@
-/**
- * --------------------------------------------------------------------------
- * CoreUI Pro Boostrap Admin Template (3.2.0): datatables.js
- * Licensed under MIT (https://coreui.io/license)
- * --------------------------------------------------------------------------
- */
-$('.datatable').DataTable();
-$('.datatable').attr('style', 'border-collapse: collapse !important');
-//# sourceMappingURL=datatables.js.map
