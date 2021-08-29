dbbed24cacb50250f915e2782f715404ab50d530
Łukasz Holeczek <lukasz@holeczek.pl>
2021-08-29 19:22:26 +0200

    update vue 4 docs

--- vue/docs/4.0/assets/js/7786.f11e187b.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_coreui_vue=self.webpackChunk_coreui_vue||[]).push([[7786],{7786:(e,t,u)=>{u.r(t),u.d(t,{default:()=>n});const n={mounted(){this.$router.push("/4.0/getting-started/introduction.html")},render:function(e,t,u,n,r,s){return null}}}}]);
