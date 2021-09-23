b9af5c3c64d5f4aba605eb009a0e8fbadce579d9
Łukasz Holeczek <lukasz@holeczek.pl>
2021-09-23 15:18:50 +0200

    docs: update react docs

--- react/docs/4.0b/component---src-pages-404-js-9879c9a7d274b71df8b3.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[43],{w2l6:function(t,e,n){"use strict";n.r(e);var i=n("aD51");e.default=function(){return Object(i.c)("div",{style:{display:"flex",alignItems:"center",justifyContent:"center",height:"100vh",width:"100vw",fontSize:32}},"Not Found")}}}]);
-//# sourceMappingURL=component---src-pages-404-js-9879c9a7d274b71df8b3.js.map
