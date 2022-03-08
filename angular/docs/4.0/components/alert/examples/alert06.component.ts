5999c44bdbf049b4726904fbd2c37c4a066994cb
xidedix <xidedix@users.noreply.github.com>
2022-03-08 02:44:13 +0100

    docs(angular): v4 (alpha.11) update alert, bvadge, carousel, smart-table

---
+++ angular/docs/4.0/components/alert/examples/alert06.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+import { cilCheck } from '@coreui/icons';
+
+@Component({
+  selector: 'docs-alert06-example',
+  templateUrl: './alert06.component.html'
+})
+export class Alert06Component {
+
+  icons = { cilCheck };
+
+}
