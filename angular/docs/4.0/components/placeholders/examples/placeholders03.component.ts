f63841e70414518c7390edb481cf8f468fc8c6a9
xidedix <xidedix@users.noreply.github.com>
2022-03-15 13:59:38 +0100

    docs(angular): v4 (alpha.15) placeholders

---
+++ angular/docs/4.0/components/placeholders/examples/placeholders03.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'docs-placeholders03',
+  templateUrl: './placeholders03.component.html',
+  styleUrls: ['./placeholders03.component.scss']
+})
+export class Placeholders03Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular/docs/4.0/components/placeholders/examples/placeholders04.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'docs-placeholders04',
+  templateUrl: './placeholders04.component.html',
+  styleUrls: ['./placeholders04.component.scss']
+})
+export class Placeholders04Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
