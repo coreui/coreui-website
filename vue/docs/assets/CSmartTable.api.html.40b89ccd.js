de74d16cda2a412bd8b428b3e8846a7d9cab2fde
Łukasz Holeczek <lukasz@holeczek.pl>
2022-04-04 16:20:17 +0200

    update vue docs

--- vue/docs/assets/CSmartTable.api.html.40b89ccd.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-203bdff8",path:"/api/smart-table/CSmartTable.api.html",title:"",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:3,title:"CSmartTable",slug:"csmarttable",children:[]}],git:{updatedTime:1647902089e3,contributors:[{name:"\u0141ukasz Holeczek",email:"lukasz@holeczek.pl",commits:3}]}};export{e as data};
