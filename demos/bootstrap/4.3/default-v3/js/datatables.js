218926beb53e7218d60ed021880f68f8bf6f99bf
mrholek <lukasz@holeczek.pl>
2022-07-12 12:54:08 +0200

    move demos to one folder

---
+++ demos/bootstrap/4.3/default-v3/js/datatables.js
@@ -0,0 +1,13 @@
+/* global $ */
+
+/**
+ * --------------------------------------------------------------------------
+ * CoreUI Boostrap Admin Template (4.2.0): datatables.js
+ * License (https://coreui.io/pro/license)
+ * --------------------------------------------------------------------------
+ */
+$(document).ready(() => {
+  $('.datatable').DataTable();
+  $('.datatable').attr('style', 'border-collapse: collapse !important');
+});
+//# sourceMappingURL=datatables.js.map
