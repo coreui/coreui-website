75af0daa45ccf72d85c0ea416af27206c0f71975
Łukasz Holeczek <lukasz@holeczek.pl>
2022-07-12 23:34:57 +0200

    clean-up

--- docs/__delete/assets/js/masked-input.js
+++
@@ -1,8 +0,0 @@
-$(function (){
-  $("#date-input").mask("99/99/9999");
-  $("#phone-input").mask("(999) 999-9999");
-  $("#tin-input").mask("99-9999999");
-  $("#ssn-input").mask("999-99-9999");
-  $("#eyescript-input").mask("~9.99 ~9.99 999");
-  $("#ccn-input").mask("9999 9999 9999 9999");
-})
