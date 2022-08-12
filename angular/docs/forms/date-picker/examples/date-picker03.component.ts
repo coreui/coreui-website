befa0d0fbe589f21e25c548791e4355087954215
xidedix <xidedix@users.noreply.github.com>
2022-08-13 00:28:32 +0200

    docs(angular): time-picker - v4.2.0-next.3

---
+++ angular/docs/forms/date-picker/examples/date-picker03.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'docs-date-picker03',
+  templateUrl: './date-picker03.component.html'
+})
+export class DatePicker03Component {
+
+}
---
+++ angular/docs/forms/date-picker/examples/date-picker12.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'docs-date-picker12',
+  templateUrl: './date-picker12.component.html'
+})
+export class DatePicker12Component {
+
+}
