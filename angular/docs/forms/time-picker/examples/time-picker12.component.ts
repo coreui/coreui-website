fbd20fe89b5c354da13814cc676ba36468c9a11e
xidedix <xidedix@users.noreply.github.com>
2022-08-19 22:57:28 +0200

    docs(angular): v4.2.0-next.5

---
+++ angular/docs/forms/time-picker/examples/time-picker12.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'docs-time-picker12',
+  templateUrl: './time-picker12.component.html',
+  styleUrls: ['./time-picker12.component.scss']
+})
+export class TimePicker12Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
