1b93463c559af0be2a7eb5833ebc695e2cbefe1e
xidedix <xidedix@users.noreply.github.com>
2022-08-25 20:04:52 +0200

    docs(angular): date-picker ng forms examples

---
+++ angular/docs/forms/date-picker/examples/date-picker13.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+interface IDatePickerForm {
+  datePicker: FormControl<Date | undefined | null>;
+}
+
+@Component({
+  selector: 'docs-date-picker13',
+  templateUrl: './date-picker13.component.html'
+})
+export class DatePicker13Component implements OnInit {
+
+  date = new Date();
+
+  formGroup!: FormGroup<IDatePickerForm>;
+
+  get toLocaleDateString() {
+    return this.formGroup.value.datePicker ? this.formGroup.value.datePicker.toLocaleDateString() : '';
+  }
+
+  ngOnInit(): void {
+    const date = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate())
+
+    this.formGroup = new FormGroup<IDatePickerForm>({
+      datePicker: new FormControl(date, { nonNullable: false })
+    });
+  }
+}
