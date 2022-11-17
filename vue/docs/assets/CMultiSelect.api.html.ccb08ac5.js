5437de060529c3ff4fd5a6420bfafb9f1613c704
Łukasz Holeczek <lukasz@holeczek.pl>
2022-11-17 15:36:53 +0100

    update vue docs

--- vue/docs/assets/CMultiSelect.api.html.ccb08ac5.js
+++
@@ -1,1 +0,0 @@
-const e={key:"v-7dbfe1aa",path:"/api/multi-select/CMultiSelect.api.html",title:"",lang:"en-US",frontmatter:{},excerpt:"",headers:[{level:3,title:"CMultiSelect",slug:"cmultiselect",children:[]}],git:{updatedTime:1667759872e3,contributors:[{name:"\u0141ukasz Holeczek",email:"lukasz@holeczek.pl",commits:3}]}};export{e as data};
