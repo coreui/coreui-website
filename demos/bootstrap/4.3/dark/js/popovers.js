db78bf01d9e3de5aa269dc11c1d61ee71ad8570f
Łukasz Holeczek <lukasz@holeczek.pl>
2022-12-03 20:00:12 +0100

    update demos

---
+++ demos/bootstrap/4.3/dark/js/popovers.js
@@ -0,0 +1,14 @@
+/* global coreui */
+
+/**
+ * --------------------------------------------------------------------------
+ * CoreUI Boostrap Admin Template (v4.3.0): popovers.js
+ * License (https://coreui.io/pro/license)
+ * --------------------------------------------------------------------------
+ */
+
+document.querySelectorAll('[data-coreui-toggle="popover"]').forEach(element => {
+  // eslint-disable-next-line no-new
+  new coreui.Popover(element);
+});
+//# sourceMappingURL=popovers.js.map
---
+++ demos/bootstrap/4.3/default/js/tooltips.js
@@ -0,0 +1,14 @@
+/* global coreui */
+
+/**
+ * --------------------------------------------------------------------------
+ * CoreUI Boostrap Admin Template (v4.3.0): tooltips.js
+ * License (https://coreui.io/pro/license)
+ * --------------------------------------------------------------------------
+ */
+
+document.querySelectorAll('[data-coreui-toggle="tooltip"]').forEach(element => {
+  // eslint-disable-next-line no-new
+  new coreui.Tooltip(element);
+});
+//# sourceMappingURL=tooltips.js.map
