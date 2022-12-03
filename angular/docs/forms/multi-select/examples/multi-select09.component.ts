c4b811ff3ef8701219687b31d55f01ae33cc84f4
xidedix <xidedix@users.noreply.github.com>
2022-12-03 04:09:17 +0100

    docs(angular): v4.2.18

---
+++ angular/docs/forms/multi-select/examples/multi-select09.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'docs-multi-select09',
+  templateUrl: './multi-select09.component.html'
+})
+export class MultiSelect09Component {
+}
