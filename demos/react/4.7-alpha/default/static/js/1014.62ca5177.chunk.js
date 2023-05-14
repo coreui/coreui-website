11fbac67d340cdeb08d43adb7e440adf8b03e33d
mrholek <lukasz@holeczek.pl>
2023-05-14 18:28:25 +0200

    update demos

---
+++ demos/react/4.7-alpha/default/static/js/1014.62ca5177.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_coreui_coreui_pro_react_admin_template=self.webpackChunk_coreui_coreui_pro_react_admin_template||[]).push([[1014],{66265:function(e,r,s){s.r(r);s(72791);var c=s(89110),a=s(80184);r.default=function(){return(0,a.jsxs)(a.Fragment,{children:[(0,a.jsx)(c.S8,{}),(0,a.jsxs)("div",{className:"wrapper d-flex flex-column min-vh-100",children:[(0,a.jsx)(c.tf,{}),(0,a.jsx)("div",{className:"body flex-grow-1 px-3",children:(0,a.jsx)(c.Lk,{})}),(0,a.jsx)(c.qh,{})]}),(0,a.jsx)(c.cM,{})]})}}}]);
+//# sourceMappingURL=1014.62ca5177.chunk.js.map
