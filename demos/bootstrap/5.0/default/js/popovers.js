f597a8f06b38ada1f4a12b830770e04569f968d1
mrholek <lukasz@holeczek.pl>
2023-07-05 19:38:11 +0200

    update demos

---
+++ demos/bootstrap/5.0/default/js/popovers.js
@@ -0,0 +1,14 @@
+/* global coreui */
+
+/**
+ * --------------------------------------------------------------------------
+ * CoreUI PRO Boostrap Admin Template popovers.js
+ * License (https://coreui.io/pro/license/)
+ * --------------------------------------------------------------------------
+ */
+
+for (const element of document.querySelectorAll('[data-coreui-toggle="popover"]')) {
+  // eslint-disable-next-line no-new
+  new coreui.Popover(element);
+}
+//# sourceMappingURL=popovers.js.map
