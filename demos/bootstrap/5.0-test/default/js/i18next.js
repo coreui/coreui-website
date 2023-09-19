5c44f89a262d7193568f77658aff96d4685f1c28
mrholek <lukasz@holeczek.pl>
2023-09-19 12:42:58 +0200

    update demos

--- demos/bootstrap/5.0-test/default/js/i18next.js
+++ demos/bootstrap/5.0-test/default/js/i18next.js
@@ -73,7 +73,7 @@ const translate = language => {
 i18next.use(i18nextHttpBackend).use(i18nextBrowserLanguageDetector).init({
   fallbackLng: 'en',
   backend: {
-    loadPath: './../locales/{{lng}}/translation.json'
+    loadPath: './locales/{{lng}}/translation.json'
   }
 }, () => {
   translate();
