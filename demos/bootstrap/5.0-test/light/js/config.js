6584f2aea33e11b7658e62468bd4450d79703774
mrholek <lukasz@holeczek.pl>
2023-09-19 15:48:36 +0200

    update demos

--- demos/bootstrap/5.0-test/light/js/config.js
+++ demos/bootstrap/5.0-test/light/js/config.js
@@ -6,7 +6,7 @@
  */
 
 (() => {
-  const THEME = 'coreui-pro-bootstrap-admin-template-theme-default';
+  const THEME = 'coreui-pro-bootstrap-admin-template-theme-light';
   const urlParams = new URLSearchParams(window.location.href.split('?')[1]);
   if (urlParams.get('theme') && ['auto', 'dark', 'light'].includes(urlParams.get('theme'))) {
     localStorage.setItem(THEME, urlParams.get('theme'));
