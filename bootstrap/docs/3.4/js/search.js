5d583d87a1d7abee83e368d16967d1c1d6c5a435
mrholek <lukasz@holeczek.pl>
2023-12-17 13:22:20 +0100

    docs: update documentation

---
+++ bootstrap/docs/3.4/js/search.js
@@ -0,0 +1,59 @@
+// NOTICE!! DO NOT USE ANY OF THIS JAVASCRIPT
+// IT'S ALL JUST JUNK FOR OUR DOCS!
+// ++++++++++++++++++++++++++++++++++++++++++
+
+(function () {
+  'use strict'
+
+  if (!window.docsearch) {
+    return
+  }
+
+  var inputElement = document.getElementById('docs-search')
+  var siteDocsVersion = inputElement.getAttribute('data-docs-version')
+
+  function getOrigin() {
+    var location = window.location
+    var origin = location.origin
+
+    if (!origin) {
+      var port = location.port ? ':' + location.port : ''
+
+      origin = location.protocol + '//' + location.hostname + port
+    }
+
+    return origin
+  }
+
+  window.docsearch({
+    apiKey: '385e4bd69f60d68d9156dc34b9289b11',
+    indexName: 'dev_COREUI',
+    inputSelector: '#docs-search',
+    algoliaOptions: {
+      facetFilters: ['version:' + siteDocsVersion]
+    },
+    transformData: function (hits) {
+      return hits.map(function (hit) {
+        var currentUrl = getOrigin()
+        var liveUrl = 'https://coreui.io/bootstrap/docs/3.0/'
+
+        // When in production, return the result as is,
+        // otherwise remove our url from it.
+        // eslint-disable-next-line no-negated-condition
+        hit.url = currentUrl.indexOf(liveUrl) !== -1 ?
+          hit.url :
+          hit.url.replace(liveUrl, '')
+
+        // Prevent jumping to first header
+        if (hit.anchor === 'content') {
+          hit.url = hit.url.replace(/#content$/, '')
+          hit.anchor = null
+        }
+
+        return hit
+      })
+    },
+    // Set debug to `true` if you want to inspect the dropdown
+    debug: false
+  })
+})()
