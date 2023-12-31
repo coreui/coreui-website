b328e287510d7db9b4ee75058609c33a0a51d42b
mrholek <lukasz@holeczek.pl>
2023-12-31 16:22:36 +0100

    update demos

---
+++ demos/next-js/2.0/light-rtl/_next/static/AV0q431N7Zpzc5Zp3qP1J/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-b225d4412fb76f89.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
