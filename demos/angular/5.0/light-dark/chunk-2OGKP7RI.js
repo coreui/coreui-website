8d7bf6519baf6b106e1795890278f060fc29e0c1
xidedix <xidedix@users.noreply.github.com>
2024-01-29 16:56:47 +0100

    demos(angular): v5.0.0-next.17 theme light, dark, rtl

---
+++ demos/angular/5.0/light-dark/chunk-2OGKP7RI.js
@@ -0,0 +1,1 @@
+import"./chunk-RLASGPT3.js";var o=[{path:"",loadComponent:()=>import("./chunk-7RNO5LSS.js").then(t=>t.SmartTablesComponent),data:{title:"Smart Table"}}];export{o as routes};
---
+++ demos/angular/5.0/light-dark/chunk-COJG7P76.js
@@ -0,0 +1,80 @@
+import{b as T}from"./chunk-GDPU53XG.js";import"./chunk-UBKFDT4A.js";import{$ as m,Ed as F,Fd as I,I as u,Jc as _,Kc as C,Lc as h,Na as e,Oa as x,Pa as b,Pc as g,Qc as y,Ta as S,Tc as B,Va as E,bb as f,cb as v,ja as d,la as l,va as t,wa as n,xa as s}from"./chunk-BOJJVPWL.js";import"./chunk-RLASGPT3.js";function L(r,i){if(r&1&&(t(0,"c-breadcrumb-item",13),e(1),n()),r&2){let a=i.$implicit,o=i.last;l("active",o)("url",a.url),m(),b(`
+              `,a.label,`
+            `)}}function D(r,i){if(r&1&&(t(0,"c-breadcrumb-item",13),e(1),n()),r&2){let a=i.$implicit,o=i.last;l("active",o)("url",a.url),m(),b(`
+              `,a.label,`
+            `)}}function O(r,i){if(r&1&&(t(0,"c-breadcrumb-item",13),e(1),n()),r&2){let a=i.$implicit,o=i.last;l("active",o)("url",a.url),m(),b(`
+              `,a.label,`
+            `)}}function H(r,i){if(r&1&&(t(0,"c-breadcrumb-item",13),e(1),n()),r&2){let a=i.$implicit,o=i.last;l("active",o)("url",a.url),m(),b(`
+              `,a.label,`
+            `)}}var w=r=>({mark:r});function $(r,i){if(r&1&&(t(0,"c-breadcrumb-item",13),e(1,`
+              `),t(2,"span",14),e(3),n(),e(4,`
+            `),n()),r&2){let a=i.$implicit,o=i.last;l("active",o)("url",a.url),m(2),l("ngClass",E(4,w,o)),m(),x(a.label)}}var N=(()=>{let i=class i{constructor(){this.items=[]}ngOnInit(){this.items=[{label:"Home",url:"/",attributes:{title:"Home"}},{label:"Library",url:"/"},{label:"Data",url:"/dashboard/"},{label:"CoreUI",url:"/"}],setTimeout(()=>{this.items=[{label:"CoreUI",url:"/"},{label:"Data",url:"/dashboard/"},{label:"Library",url:"/"},{label:"Home",url:"/",attributes:{title:"Home"}}]},5e3)}};i.\u0275fac=function(p){return new(p||i)},i.\u0275cmp=u({type:i,selectors:[["ng-component"]],standalone:!0,features:[S],decls:104,vars:7,consts:[["xs","12",1,"mb-4"],[1,"text-body-secondary","small"],["href","https://developer.mozilla.org/en-US/docs/Web/CSS/::before"],["href","https://developer.mozilla.org/en-US/docs/Web/CSS/content"],["href","components/breadcrumb"],[1,"mb-2"],[3,"active","url",4,"ngFor","ngForOf"],[1,"mb-0"],["url","/"],[3,"active"],["xs","12"],["href","components/breadcrumb#router"],[3,"items"],[3,"active","url"],[3,"ngClass"]],template:function(p,c){p&1&&(t(0,"c-row"),e(1,`
+  `),t(2,"c-col",0),e(3,`
+    `),t(4,"c-card"),e(5,`
+      `),t(6,"c-card-header"),e(7,`
+        `),t(8,"strong"),e(9,"Angular Breadcrumbs"),n(),e(10,`
+      `),n(),e(11,`
+      `),t(12,"c-card-body"),e(13,`
+        `),t(14,"p",1),e(15,`
+          The breadcrumb navigation provides links back to each previous page the user navigated
+          through and shows the current location in a website or an application. You don\u2019t have
+          to add separators, because they automatically added in CSS through
+          `),t(16,"a",2),e(17,`
+            `),t(18,"code"),e(19,"::before"),n(),e(20,`
+          `),n(),e(21,`
+          and
+          `),t(22,"a",3),e(23,`
+            `),t(24,"code"),e(25,"content"),n(),e(26,`
+          `),n(),e(27,`
+          .
+        `),n(),e(28,`
+        `),t(29,"app-docs-example",4),e(30,`
+          `),t(31,"c-breadcrumb",5),e(32,`
+            `),d(33,L,2,3,"c-breadcrumb-item",6),e(34,`
+          `),n(),e(35,`
+          `),s(36,"hr"),e(37,`
+          `),t(38,"c-breadcrumb",5),e(39,`
+            `),d(40,D,2,3,"c-breadcrumb-item",6),e(41,`
+          `),n(),e(42,`
+          `),t(43,"c-breadcrumb",5),e(44,`
+            `),d(45,O,2,3,"c-breadcrumb-item",6),e(46,`
+          `),n(),e(47,`
+          `),t(48,"c-breadcrumb",5),e(49,`
+            `),d(50,H,2,3,"c-breadcrumb-item",6),e(51,`
+          `),n(),e(52,`
+          `),t(53,"c-breadcrumb",5),e(54,`
+            `),d(55,$,5,6,"c-breadcrumb-item",6),e(56,`
+          `),n(),e(57,`
+          `),s(58,"hr"),e(59,`
+          `),t(60,"c-breadcrumb",7),e(61,`
+            `),t(62,"c-breadcrumb-item",8),e(63,`
+              Home
+            `),n(),e(64,`
+            `),t(65,"c-breadcrumb-item",8),e(66,`
+              Library
+            `),n(),e(67,`
+            `),t(68,"c-breadcrumb-item",8),e(69,`
+              Data
+            `),n(),e(70,`
+            `),t(71,"c-breadcrumb-item",9),e(72,`
+              Bootstrap
+            `),n(),e(73,`
+          `),n(),e(74,`
+        `),n(),e(75,`
+      `),n(),e(76,`
+    `),n(),e(77,`
+  `),n(),e(78,`
+  `),t(79,"c-col",10),e(80,`
+    `),t(81,"c-card"),e(82,`
+      `),t(83,"c-card-header"),e(84,`
+        `),t(85,"strong"),e(86,"Angular Breadcrumbs"),n(),e(87," "),t(88,"small"),e(89,"router"),n(),e(90,`
+      `),n(),e(91,`
+      `),t(92,"c-card-body"),e(93,`
+        `),t(94,"app-docs-example",11),e(95,`
+          `),s(96,"c-breadcrumb-router"),e(97,`
+          `),s(98,"c-breadcrumb-router",12),e(99,`
+        `),n(),e(100,`
+      `),n(),e(101,`
+    `),n(),e(102,`
+  `),n(),e(103,`
+`),n()),p&2&&(m(33),l("ngForOf",c.items),m(7),l("ngForOf",c.items.slice(0,1)),m(5),l("ngForOf",c.items.slice(0,2)),m(5),l("ngForOf",c.items.slice(0,3)),m(5),l("ngForOf",c.items.slice(0,4)),m(16),l("active",!0),m(27),l("items",c.items))},dependencies:[I,F,g,B,y,T,C,v,_,f,h]});let r=i;return r})();export{N as BreadcrumbsComponent};
