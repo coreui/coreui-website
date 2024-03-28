2db6956dc62af5cdf5bd11d0c44a4b20d510d850
mrholek <lukasz@holeczek.pl>
2024-03-28 15:12:50 +0100

    update demos

---
+++ demos/react/5.0/free/assets/CCardFooter-BsafVmzd.js
@@ -0,0 +1,1 @@
+import{r as l,_ as d,R as n,b as i,c as m,P as e}from"./index-BSRXV3W1.js";var r=l.forwardRef(function(a,s){var c=a.children,o=a.className,t=d(a,["children","className"]);return n.createElement("div",i({className:m("card-footer",o)},t,{ref:s}),c)});r.propTypes={children:e.node,className:e.string};r.displayName="CCardFooter";export{r as C};
---
+++ demos/react/5.0/free/assets/CCardGroup-wos6K5NK.js
@@ -0,0 +1,1 @@
+import{r as p,_ as t,R as d,b as n,c as i,P as r}from"./index-BSRXV3W1.js";var s=p.forwardRef(function(a,e){var c=a.children,l=a.className,o=t(a,["children","className"]);return d.createElement("div",n({className:i("card-group",l)},o,{ref:e}),c)});s.propTypes={children:r.node,className:r.string};s.displayName="CCardGroup";export{s as C};
